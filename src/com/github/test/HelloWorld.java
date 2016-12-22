a37ba9322cbb8c4db65e7965508ea06bcb8e6d96
CNazp <cn_hlj_azp@sina.com>
2016-12-22 15:03:31 +0800

    第三次提交

--- src/com/github/test/HelloWorld.java
+++ src/com/github/test/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("11111111"+"一一一一一");
-		System.out.println("22222222"+"二二二二二");
+		System.out.println("2222222"+"二二二二二");
 
 	}
 
